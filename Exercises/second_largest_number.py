aa8ca50a40776d821432b2857d2211e9e28a6c02
RahwaZeslusHaile <rahwazeslase@gmail.com>
2026-06-17 11:12:30 +0100

    Feat:a function returning the second largest number

---
+++ Exercises/second_largest_number.py
@@ -0,0 +1,13 @@
+def second_largest(numbers):
+    sorted_numbers = sorted(set(numbers))
+    return sorted_numbers[-2] if len(sorted_numbers)>=2 else None
+
+
+print(second_largest([1, 5, 3, 9, 2]))
+# 5
+
+print(second_largest([10, 10, 8, 7]))
+# 8
+
+print(second_largest([5]))
+# None)
