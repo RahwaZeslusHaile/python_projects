aff8ba26a959a8f31e45027c190f20a0e81cdeaf
RahwaZeslusHaile <rahwazeslase@gmail.com>
2026-06-23 16:24:30 +0100

    Feat:grouping by length  with two approaches

--- Exercises/group_by_length.py
+++ Exercises/group_by_length.py
@@ -1,13 +1,24 @@
+# def group_by_length(words):
+#     total = {}
+#     for word in words:
+#         length_word = len(word)
+#         if length_word not in total:
+#             total[length_word] = []
+#         total[length_word].append(word)
+#     return total
+
 def group_by_length(words):
     total = {}
     for word in words:
-        length_word = len(word)
-        if length_word not in total:
-            total[length_word] = []
-        if word not in total[length_word]:
-            total[length_word].append(word)
+        if word not in total.setdefault(len(word),[]):
+            total[len(word)].append(word)
     return total
+
+
     
 
-
+print(group_by_length([]))
 print(group_by_length(["hi", "cat", "dog", "no", "python"]))
+print(group_by_length(["hello"]))
+print(group_by_length(["hi","hi"]))
+
