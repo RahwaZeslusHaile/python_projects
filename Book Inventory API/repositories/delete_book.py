85325ec69f1fdb1db3c617397dee2f32cd91db1f
RahwaZeslusHaile <rahwazeslase@gmail.com>
2026-06-28 18:23:45 +0100

    Update:implemented deleting a specific book logic

--- Book Inventory API/repositories/delete_book.py
+++ Book Inventory API/repositories/delete_book.py
@@ -1,2 +1,15 @@
-def delete_book_by_id_from_db(id):
-    pass
+from integrations.db import get_db_connection
+
+
+def delete_book_by_id_from_db(book_id: int) -> bool:
+    query = "DELETE FROM books WHERE book_id = %s"
+    connection = get_db_connection()
+    try:
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query, (book_id,))
+
+                return cursor.rowcount > 0
+
+    finally:
+        connection.close()
