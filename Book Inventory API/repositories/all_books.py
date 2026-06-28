f22e432d62b670ce500e73004e6eb8a14264a7c4
RahwaZeslusHaile <rahwazeslase@gmail.com>
2026-06-28 18:22:35 +0100

    Update:fetching of all books from database

--- Book Inventory API/repositories/all_books.py
+++ Book Inventory API/repositories/all_books.py
@@ -1,2 +1,17 @@
+from integrations.db import get_db_connection
+
+
 def get_all_books_from_db() -> list[dict]:
-    pass
+    query = "SELECT * FROM books"
+
+    connection = get_db_connection()
+    try:
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                rows = cursor.fetchall()  # list of tuples
+                columns = [desc[0] for desc in cursor.description]
+                books = [dict(zip(columns, row)) for row in rows]
+                return books
+    finally:
+        connection.close()
