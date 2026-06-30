97c3e3322ca550473e987434dd91b49821c0ab41
RahwaZeslusHaile <rahwazeslase@gmail.com>
2026-06-30 08:42:48 +0100

    Fix:Removing of the parametre passing

--- Book Inventory API/services/get_books.py
+++ Book Inventory API/services/get_books.py
@@ -2,7 +2,7 @@ from repositories.all_books import get_all_books_from_db
 from schemas.response_validation import Books_response_validation
 
 
-def all_books_service(db_session) -> list[Books_response_validation]:
-    raw_books = get_all_books_from_db(db_session)
+def all_books_service() -> list[Books_response_validation]:
+    raw_books = get_all_books_from_db()
 
     return [Books_response_validation.model_validate(book) for book in raw_books]
