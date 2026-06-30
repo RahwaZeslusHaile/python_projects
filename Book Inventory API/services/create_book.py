91ca67478db49efdc5d6056629b713f966810748
RahwaZeslusHaile <rahwazeslase@gmail.com>
2026-06-30 08:46:35 +0100

    Fix:damp the attributes created by db and add the attributes that need pass by client

--- Book Inventory API/services/create_book.py
+++ Book Inventory API/services/create_book.py
@@ -1,13 +1,16 @@
-from datetime import datetime, timezone
-from uuid import uuid4
+from repositories.create_book import create_book_in_db
 
 from schemas.request_validation import Books_request_validation
 from schemas.response_validation import Books_response_validation
 
 
-def create_book(books: Books_request_validation) -> Books_response_validation:
-    new_book = books.model_dump()
-    new_book["id"] = str(uuid4())
-    new_book["created_at"] = datetime.now(timezone.utc)
+def create_book_service(books: Books_request_validation) -> Books_response_validation:
+    data = books.model_dump()
+    new_book = create_book_in_db(
+        title=data["title"],
+        author_id=data["author_id"],
+        price=data["price"],
+        quantity=data["quantity"]
+    )
 
-    return Books_response_validation(**new_book)
+    return Books_response_validation.model_validate(new_book)
